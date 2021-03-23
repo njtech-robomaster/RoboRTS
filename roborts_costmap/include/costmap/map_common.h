bda20ab861edf0dd74f9ee8ab5cce0914649b489
Haowei Wen <me@yushi.moe>
2021-03-24 00:08:31 +0800

    add buff zone layer

--- roborts_costmap/include/costmap/map_common.h
+++ roborts_costmap/include/costmap/map_common.h
@@ -66,7 +66,16 @@ namespace roborts_costmap {
 static const unsigned char NO_INFORMATION = 255;
 static const unsigned char LETHAL_OBSTACLE = 254;
 static const unsigned char INSCRIBED_INFLATED_OBSTACLE = 253;
+static const unsigned char BUFF_OBSTACLE = 252;
 static const unsigned char FREE_SPACE = 0;
+
+struct BuffZone {
+  double min_x_;
+  double max_x_;
+  double min_y_;
+  double max_y_;
+};
+
 } //namespace roborts_costmap
 
 #endif //ROBORTS_COSTMAP_MAP_COMMON_H
