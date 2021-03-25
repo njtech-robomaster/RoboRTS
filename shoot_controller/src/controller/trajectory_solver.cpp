a2daffa5131c6bf525ffed6fad251bc59ac88f5b
Haowei Wen <me@yushi.moe>
2021-03-26 02:13:32 +0800

    add pitch compensation & switch trajectory algorithm

--- shoot_controller/src/controller/trajectory_solver.cpp
+++ shoot_controller/src/controller/trajectory_solver.cpp
@@ -7,19 +7,66 @@ TrajectorySolver::TrajectorySolver() : tf_listener{tf_buffer} {
 	gimbal_pub = nh.advertise<roborts_msgs::GimbalAngle>("cmd_gimbal_angle", 1);
 }
 
+double calc_polynomial(const std::vector<double> &coeffs, double x) {
+	double y = 0;
+	for (size_t k = 0; k < coeffs.size(); k++) {
+		y += coeffs[k] * std::pow(x, k);
+	}
+	return y;
+}
+
+double calc_polynomial_derivative(const std::vector<double> &coeffs, double x) {
+	double y = 0;
+	for (size_t k = 1; k < coeffs.size(); k++) {
+		y += k * coeffs[k] * std::pow(x, k - 1);
+	}
+	return y;
+}
+
+double newton_iteration(const std::vector<double> &coeffs, double x0,
+                        double *err_ptr = nullptr, double max_err = 1e-6,
+                        int max_iterations = 100) {
+	int iterations = 0;
+	double y;
+	while (y = calc_polynomial(coeffs, x0), std::abs(y) > max_err) {
+		if (iterations++ > max_iterations) {
+			break;
+		}
+		x0 = x0 - y / calc_polynomial_derivative(coeffs, x0);
+	}
+	if (err_ptr != nullptr) {
+		*err_ptr = y;
+	}
+	return x0;
+}
+
+const double g = 9.8;
+
 double compute_pitch(double distance, double height, double gun_barrel_length,
                      double bullet_velocity) {
-	double a = (9.8 * (distance - gun_barrel_length) *
-	            (distance - gun_barrel_length)) /
-	           (2 * bullet_velocity * bullet_velocity);
-	double m = std::sqrt(distance * distance - 4 * a * (a + height));
-	double t1 = std::atan2(distance + m, 2 * a);
-	double t2 = std::atan2(distance - m, 2 * a);
-	if (std::abs(t1) > std::abs(t2)) {
-		return -t2;
-	} else {
-		return -t1;
+	std::vector<double> coeffs = {
+	    -2 * g * gun_barrel_length * distance +
+	        2 * height * bullet_velocity * bullet_velocity +
+	        g * distance * distance + g * gun_barrel_length * gun_barrel_length,
+	    -4 * bullet_velocity * bullet_velocity * distance,
+	    -2 * g * gun_barrel_length * distance + 3 * g * distance * distance -
+	        2 * height * bullet_velocity * bullet_velocity -
+	        g * gun_barrel_length * gun_barrel_length,
+	    +8 * bullet_velocity * bullet_velocity * distance,
+	    +2 * g * gun_barrel_length * distance + 3 * g * distance * distance -
+	        2 * height * bullet_velocity * bullet_velocity -
+	        g * gun_barrel_length * gun_barrel_length,
+	    -4 * bullet_velocity * bullet_velocity * distance,
+	    +2 * g * gun_barrel_length * distance +
+	        2 * height * bullet_velocity * bullet_velocity +
+	        g * distance * distance +
+	        g * gun_barrel_length * gun_barrel_length};
+	double err;
+	double k = newton_iteration(coeffs, 0, &err);
+	if (err > 1e-3) {
+		return NAN;
 	}
+	return 2 * std::atan(k);
 }
 
 bool TrajectorySolver::aim_target(const geometry_msgs::PointStamped &target_) {
@@ -29,10 +76,12 @@ bool TrajectorySolver::aim_target(const geometry_msgs::PointStamped &target_) {
 	shoot_in_pitch_base.header.frame_id = shoot_frame;
 	shoot_in_pitch_base.header.stamp = target_.header.stamp;
 	try {
-		tf_buffer.transform(target_, target_in_pitch_base, pitch_base_frame);
-		tf_buffer.transform(target_, target_in_yaw_base, yaw_base_frame);
+		tf_buffer.transform(target_, target_in_pitch_base, pitch_base_frame,
+		                    ros::Duration{0.010});
+		tf_buffer.transform(target_, target_in_yaw_base, yaw_base_frame,
+		                    ros::Duration{0.010});
 		tf_buffer.transform(shoot_in_pitch_base, shoot_in_pitch_base,
-		                    pitch_base_frame);
+		                    pitch_base_frame, ros::Duration{0.010});
 	} catch (const std::exception &e) {
 		ROS_WARN("Couldn't lookup transform: %s", e.what());
 		return false;
@@ -42,17 +91,22 @@ bool TrajectorySolver::aim_target(const geometry_msgs::PointStamped &target_) {
 	              shoot_in_pitch_base.point.y * shoot_in_pitch_base.point.y +
 	              shoot_in_pitch_base.point.z * shoot_in_pitch_base.point.z);
 
-	roborts_msgs::GimbalAngle gimbal_ctrl;
-	gimbal_ctrl.yaw_mode = false;
-	gimbal_ctrl.pitch_mode = false;
-
-	gimbal_ctrl.yaw_angle =
-	    std::atan2(target_in_yaw_base.point.y, target_in_yaw_base.point.x);
-	gimbal_ctrl.pitch_angle = compute_pitch(
+	double pitch = -compute_pitch(
 	    std::sqrt(target_in_yaw_base.point.x * target_in_yaw_base.point.x +
 	              target_in_yaw_base.point.y * target_in_yaw_base.point.y),
 	    target_in_pitch_base.point.z, gun_barrel_length, bullet_velocity);
+	if (std::isnan(pitch)) {
+		return false;
+	}
+	double pitch_compensation = 0;
+	ros::param::getCached("~pitch_compensation", pitch_compensation);
 
+	roborts_msgs::GimbalAngle gimbal_ctrl;
+	gimbal_ctrl.yaw_mode = false;
+	gimbal_ctrl.pitch_mode = false;
+	gimbal_ctrl.yaw_angle =
+	    std::atan2(target_in_yaw_base.point.y, target_in_yaw_base.point.x);
+	gimbal_ctrl.pitch_angle = pitch - pitch_compensation;
 	gimbal_pub.publish(gimbal_ctrl);
 
 	return true;
---
+++ shoot_controller/src/pitch_calibration/pitch_calibration.cpp
@@ -0,0 +1,54 @@
+#include <roborts_msgs/GimbalAngle.h>
+#include <ros/ros.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2_ros/transform_listener.h>
+
+int main(int argc, char **argv) {
+	ros::init(argc, argv, "pitch_calibration");
+
+	double start = -.4;
+	double end = .4;
+	double step = .001;
+
+	double ctrl = NAN;
+
+	tf2_ros::Buffer tf_buf;
+	tf2_ros::TransformListener tf_listener{tf_buf};
+	ros::NodeHandle nh;
+	ros::Publisher pub =
+	    nh.advertise<roborts_msgs::GimbalAngle>("cmd_gimbal_angle", 1);
+
+	ros::Rate loop(10);
+	while (ros::ok()) {
+		ros::spinOnce();
+		loop.sleep();
+
+		if (std::isnan(ctrl)) {
+			ctrl = start;
+		} else {
+			geometry_msgs::TransformStamped transform;
+			try {
+				transform = tf_buf.lookupTransform(
+				    "pitch_link", "pitch_base_link", ros::Time::now(),
+				    ros::Duration(0.050));
+			} catch (const std::exception &e) {
+				ROS_WARN("%s", e.what());
+				continue;
+			}
+			auto q = transform.transform.rotation;
+			double roll, pitch, yaw;
+			tf2::Matrix3x3{{q.x, q.y, q.z, q.w}}.getRPY(roll, pitch, yaw);
+			ROS_INFO("ccc %lf %lf", ctrl, -pitch);
+			ctrl += step;
+		}
+
+		if (ctrl > end) {
+			break;
+		}
+
+		roborts_msgs::GimbalAngle msg;
+		msg.pitch_mode = false;
+		msg.pitch_angle = ctrl;
+		pub.publish(msg);
+	}
+}
