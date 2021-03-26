bf9f2afb85046e7d118da75b82e70c24d68e2923
Haowei Wen <me@yushi.moe>
2021-03-27 03:04:24 +0800

    set team_color&robot_id param only when id changes

--- robot_info_publisher/src/main.cpp
+++ robot_info_publisher/src/main.cpp
@@ -5,9 +5,14 @@ int main(int argc, char **argv) {
 	ros::init(argc, argv, "robot_info_publisher");
 	ros::NodeHandle nh;
 
+	uint8_t robot_id = 0;
 	auto robot_status_sub = nh.subscribe<roborts_msgs::RobotStatus>(
 	    "robot_status", 1, [&](const roborts_msgs::RobotStatus::ConstPtr &msg) {
-		    switch (msg->id) {
+		    if (msg->id == robot_id) {
+			    return;
+		    }
+		    robot_id = msg->id;
+		    switch (robot_id) {
 		    case 1:
 			    ros::param::set("team_color", "red");
 			    ros::param::set("robot_id", 1);
@@ -25,7 +30,7 @@ int main(int argc, char **argv) {
 			    ros::param::set("robot_id", 2);
 			    break;
 		    default:
-			    ROS_WARN("Unrecognized robot id: %d", msg->id);
+			    ROS_WARN("Unrecognized robot id: %d", robot_id);
 			    break;
 		    }
 	    });
